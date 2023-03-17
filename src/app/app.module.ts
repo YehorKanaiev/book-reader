47ad53aa4d316c64e9302480fa034feb9197886f
Yehor Kanaiev <egor15kanaev@gmail.com>
2023-03-17 12:07:29 +0200

    feat: add material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '@reader/app.component';
 import { ReaderModule } from '@reader/reader/reader.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ReaderModule],
+  imports: [BrowserModule, AppRoutingModule, ReaderModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
